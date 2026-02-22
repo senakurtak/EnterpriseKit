6471d1cefbed737c3e2c80b635cebd0e75cc6641
Sena Kurtak <zskurtak@gmail.com>
2026-02-22 03:54:18 +0300

    Initial Commit

---
+++ EnterpriseKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnterpriseKit
+//
+//  Created by Sena Kurtak on 22.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
